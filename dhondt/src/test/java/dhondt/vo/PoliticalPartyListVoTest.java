accee768cab088c0a62ed3e76a785c8cbab1d5ec
masanori soeda <m.soepy@gmail.com>
2021-12-09 10:04:48 +0900

    投票結果のVOクラスをテスト駆動開発

--- dhondt/src/test/java/dhondt/vo/PoliticalPartyListVoTest.java
+++ dhondt/src/test/java/dhondt/vo/PoliticalPartyListVoTest.java
@@ -1,14 +1,35 @@
 package dhondt.vo;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PoliticalPartyListVoTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private PoliticalPartyListVo it;
+
+	@BeforeEach
+	void setup() {
+		String[][] politicalPartys = { { "©–¯“}", "10000" } };
+		it = new PoliticalPartyListVo(politicalPartys);
 	}
 
+	@Test
+	@DisplayName("­“}–¼‚ªæ“¾‚Å‚«‚é")
+	void testGetName() {
+		List<PoliticalPartyVo> actual = it.getList();
+		assertThat(actual.get(0).getName(), is("©–¯“}"));
+	}
+
+	@Test
+	@DisplayName("“Š•[”‚ªæ“¾‚Å‚«‚é")
+	void testGetVotes() {
+		List<PoliticalPartyVo> actual = it.getList();
+		assertThat(actual.get(0).getVotes(), is(10000L));
+	}
 }
